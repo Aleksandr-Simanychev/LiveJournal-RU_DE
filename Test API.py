bda02a2a097bc68e87fbf80086139be30bf5b6cf
Aleksandr Simanychev <80710558+Aleksandr-Simanychev@users.noreply.github.com>
2021-05-20 19:21:21 +0300

    Update Test API.py

--- Test API.py
+++ Test API.py
@@ -1,24 +1,24 @@
 import csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options = webdriver.ChromeOptions()                                                 #Ввиду того, что в ЖЖ на некоторых публикациях стоит ограничение, которое
+options.add_argument("start-maximized")                                             #скрывает часть страницы, до тех пор пока не будет задействован триггер, мне
+options.add_experimental_option("excludeSwitches", ["enable-automation"])           #пришлось эмулировать запуск страницы с Google Chrome с помощью биб. selenium
 options.add_experimental_option('useAutomationExtension', False)
-Previous_Post = 'https://ru-de.livejournal.com/64938.html'
-while True:
+Previous_Post = 'https://ru-de.livejournal.com/64938.html'                          #Выкачивание публикаций начинается с последнего поста и идет в цикле 
+while True:                                                                         #по предыдущим, пока url страницы не совпадет с url первого поста (38 строчка)
     print(Previous_Post)
     browser = webdriver.Chrome(options=options, executable_path=r'/Users/aleksandrsimanychev/Downloads/chromedriver')
     browser.get(url = Previous_Post)
     soup = BeautifulSoup(browser.page_source, 'lxml')
-    with open('LJ.csv', 'a') as csvfile:
-        fieldnames = ['post', 'date']
+    with open('LJ.csv', 'a') as csvfile:                                            #Полученные публикации будут записываться прямо в csv таблицу 
+        fieldnames = ['post', 'date']                                               #со столбцами post(тело публикации) - date(год публикации)
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        if '<a class="_w9" href="' in str(soup):
-            posts = soup.find_all('div', class_='aentry-post__text aentry-post__text--view')
-            comments = soup.find_all('div', class_='mdspost-comment__body')
-            Year_Of_Post = str(soup).partition('<time>')[2].partition('</time>')[0]
-            Year_Of_Post = '20' + Year_Of_Post.partition('20')[2].partition(', ')[0]
+        if '<a class="_w9" href="' in str(soup):                                                    #На ЖЖ есть два вида оформления постов, которые имеют совершенно
+            posts = soup.find_all('div', class_='aentry-post__text aentry-post__text--view')        #разный исходный код.
+            comments = soup.find_all('div', class_='mdspost-comment__body')                         #По конкретным фрагментов кода можно определить вид оформления и
+            Year_Of_Post = str(soup).partition('<time>')[2].partition('</time>')[0]                 #дальше выкачивать необходимую информацию 
+            Year_Of_Post = '20' + Year_Of_Post.partition('20')[2].partition(', ')[0]                #с помощью биб. BeautifulSoup. RegEx показал меньшую эффективность
             Previous_Post = str(soup).partition('<a class="_w9" href="')[2].partition('" target')[0]
         else:
             posts = soup.find_all('div', class_='asset-body')
